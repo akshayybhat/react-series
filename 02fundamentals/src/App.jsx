8aacb7b83c1c1ab29d4a272602da1b8e3a1dbb2c
akshayybhat <akshayybhat6@gmail.com>
2025-05-11 15:40:47 +0530

    added all fundamentals

--- 02fundamentals/src/App.jsx
+++ 02fundamentals/src/App.jsx
@@ -1,46 +1,27 @@
-
+import { books } from "./books"
 
 function App() {
-  const books = [
-    {
-      img: 'https://images-na.ssl-images-amazon.com/images/I/91ZVf3kNrcL._AC_UL600_SR600,400_.jpg',
-      author:"Mel Robbins",
-      title: "The Let Them Theory",
-      id: 1,
-    },
-    {
-      img: 'https://images-na.ssl-images-amazon.com/images/I/61o5Q8IIq4L._AC_UL600_SR600,400_.jpg',
-      author: "Suzanne Collins",
-      title:"Sunrise on the Reaping ",
-      id: 2,
-      
-    },
-    {
-      img: 'https://images-na.ssl-images-amazon.com/images/I/71fD+iFwL1L._AC_UL600_SR600,400_.jpg',
-      author:"Emily Henry ",
-      title: "Sunrise on the Reaping",
-      id: 3,
-    },
-  ]
-
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-3 align-middle">
-      {books.map((book)=>{
-
-        return (
-          <article key={book.id}>
-            <Book {...book} />
-          </article>
-        )
-      })}
+    <div>
+      <h1 className="font-bold">Famous Books</h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-3 align-middle">
+        {books.map((book, index)=>{
+          return (
+            <article key={book.id}> 
+              <Book {...book} index={index}/>
+            </article>
+          )
+        })}
+      </div>
     </div>
   )
 }
 
-const Book = ({img,author, title}) =>{
-  
+const Book = ({img,author, title, index}) =>{
+ 
   return (
     <section className="bg-amber-100">
+      <p>{index+1}</p>
       <div className="m-2 p-3 text-center">
         <img src={img} alt={title} className="mx-auto" />
         <h2 className="font-bold mt-2">{title}</h2>
---
+++ 02fundamentals/src/books.js
@@ -0,0 +1,21 @@
+export const books = [
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/91ZVf3kNrcL._AC_UL600_SR600,400_.jpg',
+    author: "Mel Robbins",
+    title: "The Let Them Theory",
+    id: 1,
+  },
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/61o5Q8IIq4L._AC_UL600_SR600,400_.jpg',
+    author: "Suzanne Collins",
+    title: "Sunrise on the Reaping ",
+    id: 2,
+
+  },
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71fD+iFwL1L._AC_UL600_SR600,400_.jpg',
+    author: "Emily Henry ",
+    title: "Great Big Beautiful Life",
+    id: 3,
+  },
+]
